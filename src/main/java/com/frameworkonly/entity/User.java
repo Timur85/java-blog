c75df1d7bba9be695f8f9afd89489faac0a4ef83
Timur <mtimur0385@gmail.com>
2016-04-21 15:14:18 +0500

    commit4

--- src/main/java/com/frameworkonly/entity/User.java
+++ src/main/java/com/frameworkonly/entity/User.java
@@ -23,7 +23,8 @@ public class User {
 	@Size(min=3, message="Name must be at least 3 characters")
 	private String name;
 	
-	@Email
+	@Size(min=1, message="Invalid email address!")
+	@Email(message="Invalid email address!")
 	private String email;
 	
 	@Size(min=5, message="Password must be at least 3 characters")
